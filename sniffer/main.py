ff1d867f0d6fe750916dc63acb79267c35551e8b
Anastasiia Balai <52247243+hikkarin@users.noreply.github.com>
2021-12-15 17:40:35 +0200

    Update main.py

--- sniffer/main.py
+++ sniffer/main.py
@@ -1,19 +1,14 @@
 import socket
 import struct
 
-# the public network interface
 HOST = socket.gethostbyname("")
 # create a raw socket and bind it to the public interface
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
 
 s.bind((HOST, 0))
 
-# Include IP headers
 s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-# receive all packages
-
-# receive a package
 n = 1
 while (n <= 400):
     print('Number ', n)
