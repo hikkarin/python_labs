541789d3483e116e6ca8a3314e66c2d9da028c92
hikkarin <anastasiia.balai@nure.ua>
2021-12-21 16:55:23 +0200

    UPDATE sniffer

--- sniffer/main.py
+++ sniffer/main.py
@@ -2,7 +2,7 @@ import socket
 import struct
 
 HOST = socket.gethostbyname("")
-# create a raw socket and bind it to the public interface
+
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
 
 s.bind((HOST, 0))
@@ -11,9 +11,12 @@ s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
 n = 1
 while (n <= 400):
-    print('Number ', n)
+    print('RESULT:\n')
     data = s.recvfrom(3000)
     packet = data[0]
     address = data[1]
     header = struct.unpack('!BBHHHBBHBBBBBBBB', packet[:20])
-    print(address, packet, header)
+    print("IP:" + str(address[0]) + "\n")
+    print(str(packet) + "\n")
+    print("PORT:" + str(header[3]) + "\n")
+
